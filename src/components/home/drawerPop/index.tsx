e32b0bca50a94a8514c1875dfc3ca2b32258a82c
Biprodeep Bose <bipbose123@gmail.com>
2025-06-20 03:14:14 +0530

    poj

--- src/components/home/drawerPop/index.tsx
+++ src/components/home/drawerPop/index.tsx
@@ -1,13 +1,15 @@
 import { useProduct } from "@/Context/productContext";
 
 const DrawerPop = () =>{
-    const { barcode } = useProduct();
+    const { barcode, product } = useProduct();
     return(
-        <div className={`fixed bg-white inset-0 flex items-center ${
+        <div className={`fixed bg-white inset-0 flex transition-all duration-300 ${
 				barcode ? "translate-x-0" : "translate-x-full"
 			}`}>
-            <div className="flex flex-col max-w-[1440px] w-full">
+            <div className="flex flex-col max-w-[1440px] w-full items-center">
+                <div className="flex flex-row gap-2 font-bold text-4xl my-5">Food<span className="text-emerald-400">Draft</span></div>
                 {barcode}
+                {product?.productName}
             </div>
         </div>
     )
